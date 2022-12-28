fa044e479e7f49ca73584ffc2d683ffbdc4718ce
MR-BOTZt <120255966+MR-BOTZt@users.noreply.github.com>
2022-12-28 07:23:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,9 +10,8 @@ class Development(Config):
     # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
     DB_URI = "postgres://hwffqxra:LZ0wJmLXRv9rCsgTJf5Cnp-i1lqU2YCl@mahmud.db.elephantsql.com/hwffqxra"
     # After cloning the repo and installing requirements do python3 telesetup.py an fill th this
-    LEGEND_STRING = "1AZWarzkBuwowz5K-jlapMGu4ryKFqw0zA6zQJeyIZmh5Tssh3TvQM-kcCyb-sKRLusuxoEqQV0XLQQdkNiJGmU3NDviJGauxsX75_WZODcCM7rQ8nbYHqtSlEmLAHjNoAofxIAKCENwiJw0xK-JW4UWOtqH5Ti_OMA6j2iGvEH0s3WwOoSFfW97nMd_IWZQCPmWt-1nkSyERsc6MBS1s7LNH3aRZFGsSck_mGtgHwZuaiH8tbkcPMkHOyfEjTG58bkB_HQSMzrQSgWRukDBrhoNGMXUNhvkYFdL6QYOAL81P4jvLVZT58R2drlI7oBcp3zrT_C00lO7ZqwyNdm31uLCvccCPYBY="
-    # create a new bot in @botfather and fill the following vales with bottoken
-    BOT_TOKEN = "5967121399:AAHLD_XwKub0SXhqqB3NSpQTLKTqPM2oDqw"
+    LEGEND_STRING = "BQAAHmJ14H_E4V1LsGV4hTCjLiN6jqkJp7ndnKkuExuaFOXECEImAj2XXz8qk6ozm684Nj-WMLP_gfoO4CvHko_k77UH25B8pTqpdXEkHFlqS1V-JEeWyRpEbZCaNKIroj9NNKCa0ypCWQLesxWqG084yt3_trPwAEsHgsPScKSlyuCON7mwywfkvIZd6CaWKjuOWjueJV320Id8AF-P665IrQjiSxTiQ6AwNfCxLfRoBi-8bQ5lx0-Q7JTAWiDywugpJLEw8uPvcXd9OiZkFoetslDc5Y_bQXN8d0Qw3lNCdFY1zB2YBIlJPEvjD9zelvGz_s1KvbbWcpUo6d0AffUOAAAAAVpJUgMA"
+    BOT_TOKEN = "5813854974:AAGk1ZVF8tGuDigs47Cx_pArPggz1cl7Po8"
     # command handler
     HANDLER = "."
     # command hanler for sudo
@@ -22,6 +21,5 @@ class Development(Config):
     EXTERNAL_REPOBRANCH = "main"
     UPSTREAM_REPO = "pro"
     VCMODE = True
-    VC_SESSION = "1AZWarzoBu71TvpnUiO4BkIW0maj8Zsr0-r45iY4fSQ_fMf7W4plbaV5QSsJsHoFhOrZmxOYe2-XecXb_-KKXx3qMNjBAWAhu1eEFtPlOtaziOwyBdBBWc1im5yWOeaRkBoCh97yx9MMxUKBlFfNULsOXLzMevXd0-5D_-c53R0WOecZRs5G4nuRSa_G5aoe1LXdrmzhnJNLG4YDk0sRWwr9Uj7jRO_pVtVXD4BD4ug457JvDGPq1LRgxuo_19qbcNsX4_AuaXbE3KJ496VPc-VW8sQJWh5c99DFlHjUCz-fzIbD6AbETpiHzGv4zfRYHvh1U9sqOypRzMg9e3Y5hcBu3bbXQ34U="
-    # Your City's TimeZone
+    VC_SESSION = "BQAAHmJ14H_E4V1LsGV4hTCjLiN6jqkJp7ndnKkuExuaFOXECEImAj2XXz8qk6ozm684Nj-WMLP_gfoO4CvHko_k77UH25B8pTqpdXEkHFlqS1V-JEeWyRpEbZCaNKIroj9NNKCa0ypCWQLesxWqG084yt3_trPwAEsHgsPScKSlyuCON7mwywfkvIZd6CaWKjuOWjueJV320Id8AF-P665IrQjiSxTiQ6AwNfCxLfRoBi-8bQ5lx0-Q7JTAWiDywugpJLEw8uPvcXd9OiZkFoetslDc5Y_bQXN8d0Qw3lNCdFY1zB2YBIlJPEvjD9zelvGz_s1KvbbWcpUo6d0AffUOAAAAAVpJUgMA"    # Your City's TimeZone
     TZ = "Asia/Kolkata"
